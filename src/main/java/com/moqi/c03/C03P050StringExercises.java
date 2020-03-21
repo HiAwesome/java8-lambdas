42872182bc15126205d062c896df8a62b98937ca
moqi <moqimoqidea@gmail.com>
2020-03-21 18:35:12 +0800

    Update

--- src/main/java/com/moqi/c03/C03P050StringExercises.java
+++ src/main/java/com/moqi/c03/C03P050StringExercises.java
@@ -6,17 +6,21 @@ import java.util.Optional;
 
 public class C03P050StringExercises {
 
-    // Question 6
+    /**
+     * 计算一个字符串中小写字母的个数（提示：参阅 String 对象的 chars 方法）。
+     */
     public static int countLowercaseLetters(String string) {
         return (int) string.chars()
                 .filter(Character::isLowerCase)
                 .count();
     }
 
-    // Question 7
+    /**
+     * 在一个字符串列表中， 找出包含最多小写字母的字符串。 对于空列表， 返回 Optional<String> 对象。
+     */
     public static Optional<String> mostLowercaseString(List<String> strings) {
-        return strings.stream()
-                .max(Comparator.comparing(C03P050StringExercises::countLowercaseLetters));
+        return strings.stream().max(Comparator.comparing(
+                C03P050StringExercises::countLowercaseLetters));
     }
 
 }
